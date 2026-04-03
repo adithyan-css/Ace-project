7b6bfb026868d5629683c50578c7002fdb18464f
adithyan <adithyansubramani1@gmail.com>
2026-04-03 16:41:58 +0530

    feat: update leaflet styles for improved map visibility and aesthetics

--- frontend/src/components/MapView.jsx
+++ frontend/src/components/MapView.jsx
@@ -69,9 +69,8 @@ const MapView = () => {
 
       <MapContainer key={mapKey} center={position} zoom={15} style={{ height: '100%', width: '100%' }} zoomControl={false}>
         <TileLayer
-          url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-          subdomains="abcd"
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           maxZoom={20}
         />
 
